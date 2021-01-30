4d66bb63df3b09763fbdcfc4a93e4887b5987568
takumi425 <fujishima.t425@gmail.com>
2021-01-30 18:54:24 +0900

    武器の操作を担当するWeaponManagerクラスを作成しました

--- Assets/Script/Weapon/WeaponBase.cs
+++ Assets/Script/Weapon/WeaponBase.cs
@@ -1,6 +1,6 @@
 ﻿//武器の基礎的な情報を保持する基底クラス
 //武器のクラスはこのクラスを継承して作成する
-abstract class WeaponBase
+ public abstract class WeaponBase
 {
     //武器の攻撃力
     protected int _weaponAtackValue;
@@ -15,7 +15,7 @@ abstract class WeaponBase
 
 
     //各武器の攻撃を処理する関数
-    public abstract void WeaponAtack();
+    public abstract void WeaponAction();
 }
 
 public enum WEAPONE_ID
