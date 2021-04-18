c238009458468536fc3000c136eda8f34ad85285
takumi425 <fujishima.t425@gmail.com>
2021-04-18 14:26:15 +0900

    効果音を再生させるSEPlayerスクリプトを作成しました

---
+++ Assets/Script/Audio/SEPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//エラーを発生させないようにするため
+[RequireComponent(typeof(AudioSource))]
+public class SEPlayer : MonoBehaviour
+{
+    /// <summary>
+    ///　再生されるSEの配列
+    /// </summary>
+    [SerializeField] AudioClip[] _seClips;
+    AudioSource _audio;
+
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// SEの中からランダムに再生する
+    /// </summary>
+    //TODO:連続して呼ばれたら再生中のSEを止める
+    public void PlaySE()
+    {
+        if (_seClips.Length == 0) return;
+
+        int seIndex = Random.Range(0, _seClips.Length);
+        _audio.PlayOneShot(_seClips[seIndex]);
+    }
+
+
+}
