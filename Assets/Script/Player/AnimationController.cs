2e4aeca32613ac03e0b2c8bcb28ccb527fa6b21b
takumi425 <fujishima.t425@gmail.com>
2021-04-19 14:29:37 +0900

    プレイヤーのアニメーションを制御するスクリプトを作成しました

---
+++ Assets/Script/Player/AnimationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Animator _animator;
+    
+    /// <summary>プレイヤーの移動判定に使用</summary>
+    Rigidbody _rb;
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animatorのSpeedにプレイヤーの移動速度をセットする
+        _animator.SetFloat("Speed", _rb.velocity.magnitude);
+    }
+}
+
+
+
